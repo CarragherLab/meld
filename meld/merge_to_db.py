cec306f21ff9427bd6fc34643af58ac9c049564a
Scott Warchal <s.warchal@sms.ed.ac.uk>
2018-02-26 14:53:40 +0000

    better docs and check python version

--- meld/merge_to_db.py
+++ meld/merge_to_db.py
@@ -3,6 +3,7 @@ Class to merge output files to tables within an sqlite database
 """
 
 import os
+import sys
 import warnings
 import pandas as pd
 import sqlalchemy
@@ -14,7 +15,7 @@ from meld import utils
 class Merger(object):
 
     """
-    Directory containing the .csv from a CellProfiler run
+    Collect and merge distributed cellprofiler results into an sqlite database.
 
     Methods
     -------
@@ -30,7 +31,17 @@ class Merger(object):
 
     def __init__(self, directory):
         """
-        Get full filepaths of all files in a directory, including sub-directories
+        Get full filepaths of all files in a directory, including
+        sub-directories.
+
+        Parameters:
+        ------------
+        directory: string
+            Path to results directory containing sub-directories of results
+
+        Returns:
+        ---------
+        Nothing
         """
         if not os.path.isdir(directory):
             raise NotADirectoryError("{} is not a directory".format(directory))
@@ -63,7 +74,10 @@ class Merger(object):
         Note:
         ------
         If a database already exists with the same location and name then
-        this will act on the existing database rather than overwriting
+        this will act on the existing database rather than overwriting.
+        If the database contains existing tables of the same name, then these
+        will be appended to if possible. If they have different column names,
+        then an sqlalchemy error will be returned.
         """
         if not db_name.lower().endswith((".sqlite", ".sqlite3")):
             db_name += ".sqlite"
@@ -83,12 +97,19 @@ class Merger(object):
         select : string
             the name of the .csv file, this will also be the database table
         header : int or list
-            the number of header rows.
+            the number of header rows, i.e. rows of column names.
         **kwargs : additional arguments to pandas.read_csv
 
         Returns:
         --------
         Nothing, writes to databases or raises an Error
+
+        Note:
+        ------
+        This will attempt to append to a database table if one exists with the
+        same name and the same column headers. The an existing database table
+        under the same name exists, but has different column headers then an
+        sqlalchemy error will be raised.
         """
         if self.engine is None or self.db_handle is None:
             msg = "no database found, need to call create_db() first"
@@ -115,8 +136,10 @@ class Merger(object):
                 if isinstance(all_file.columns, pd.core.index.MultiIndex):
                     all_file.columns = colfuncs.collapse_cols(all_file)
                 else:
-                    msg = "Multiple headers selected, yet dataframe is not multi-indexed, try with 'header=0'"
-                    raise HeaderError(msg)
+                    raise HeaderError(
+                        "Multiple headers selected, yet dataframe is not "
+                        + "multi-indexed, try with 'header=0'"
+                    )
                 # write to database
                 all_file.to_sql(select, con=self.engine, index=False,
                                 if_exists="append")
@@ -148,6 +171,17 @@ class Merger(object):
         Returns:
         --------
         Nothing, writes to database or raises an Error
+        
+        Note:
+        ------
+        This will attempt to append to a database table if one exists with the
+        same name and the same column headers. The an existing database table
+        under the same name exists, but has different column headers then an
+        sqlalchemy error will be raised.
+
+        The database tables will be named the same as `select`, but appended
+        with '_agg', e.g if `select='DATA'', then the table will be named
+        `DATA_agg`.
         """
         if self.engine is None or self.db_handle is None:
             msg = "no database found, need to call create_db() first"
@@ -174,8 +208,10 @@ class Merger(object):
                     # user has passed multiple header rows, but pandas doesn't
                     # think the dataframe has multi-indexed columns so return
                     # an error
-                    raise HeaderError("Multiple headers selected, yet dataframe\
-                                       is not multi-indexed, try with 'header=0'")
+                    raise HeaderError(
+                        "Multiple headers selected, yet dataframe is not "
+                        + "multi-indexed, try with 'header=0'"
+                    )
                 tmp_agg = utils.aggregate(tmp_file, on=by, method=method,
                                           **kwargs)
                 tmp_agg.to_sql(select + "_agg", con=self.engine, index=False,
@@ -187,6 +223,8 @@ class HeaderError(Exception):
     pass
 
 
-class NotADirectoryError(Exception):
-    """Doesn't exist in python2, create our own"""
-    pass
+if sys.version_info.major < 3:
+    # running on python2, create NotADirectoryError class
+    class NotADirectoryError(Exception):
+        """Doesn't exist in python2, create our own"""
+        pass
