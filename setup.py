f69c1be039ee01e6630a62b9c46da53b1035037c
Scott Warchal <S.Warchal@sms.ed.ac.uk>
2018-02-27 21:58:54 +0000

    fix weird indentation

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup
 
 def read_list(filepath):
-      with open(filepath, "r") as f:
-            rows = f.readlines()
-            rows = [line.strip() for line in rows]
-      return rows
+    with open(filepath, "r") as f:
+        rows = f.readlines()
+        rows = [line.strip() for line in rows]
+    return rows
 
 
-setup(name="meld",
-      version="0.0.1",
-      url="http://github.com/swarchal/meld",
-      description="collating cellprofiler data from eddie jobs",
-      license="MIT",
-      packages=["meld"],
-      tests_require=["pytest"],
-      install_requires=read_list("requirements.txt"))
+setup(
+    name="meld",
+    version="0.0.1",
+    url="http://github.com/swarchal/meld",
+    description="collating cellprofiler data from eddie jobs",
+    license="MIT",
+    packages=["meld"],
+    tests_require=["pytest"],
+    install_requires=read_list("requirements.txt")
+)
